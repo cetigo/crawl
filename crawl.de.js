d28bddf86c89f220b2e97f0cd5ebc8f817b77820
jkunstwald <jkunstwald@gmail.com>
2014-11-28 19:53:09 +0100

    Initial Commit
    Everything Uploaded, Version 0.3

---
+++ crawl.de.js
@@ -0,0 +1,175 @@
+lang = {
+	intro0 : 'Der Regen wird schwerer.<br>Dein Kopf summt vom Aufschlag.<br>Dunkelheit umgibt dich.<br>Ein fahler Geruch.',
+	intro1 : "Der Felsboden ist kalt.<br>Du erhebst dich langsam.<br>Kein Licht.<br>Niemand.",
+	intro2 : 'Retinale Adaptation.<br>Ein Minenschacht.<br>Erloschene Fackel an der Wand.<br>Flucht.',
+	intro3 : '<br>Du gewinnst <em>Bewusstsein</em> &uuml;ber dich.<br><br><span id="text-trigger1">Wunden Verbinden</span>',
+	intro4 : 'Ein St&uuml;ck deines Hemdes.<br><i>Notd&uuml;rftig. <em>Nicht desinfiziert</em></i><br>Brennender Schmerz.<br>Die Blutung l&auml;sst nach.<br>',
+	intro5a : 'Du siehst dich in der Kammer um.<br>Leer. Mit Holzpfeilern befestigt.<br><i>Es gibt ',
+	intro5b : ' angrenzende R&auml;ume.<br><i>W A S D.</i>',
+
+	enemyStart : ' taucht auf.',
+	roomChest : '<br>Du findest eine <span id="text-loader2" data-text="Truhe">Truhe</span>.',
+	roomChestLocked : '<br>Du findest eine Truhe.<br>Sie ist verschlossen.',
+	roomChestLockedOption : '<br>Du findest eine Truhe.<br>Sie ist verschlossen.<br><br><span id="text-trigger1">Aufschlie&szlig;en</span>',
+	roomChestUnlocked : '<br>Die Truhe ist ge&ouml;ffnet.<br>',
+	roomRelic : '<br>Ein altes Relikt steht in der Mitte des Raums.',
+	roomFire : '<br>Das knisternde Feuer erhellt die Kammer.<br>Rast an der Feuerstelle regeneriert dich.',
+	roomFireNow : '<br>Das knisternde Feuer erhellt die Kammer.<br>Du <span id="text-loader" data-text="erholst dich">erholst dich</span> an der Feuerstelle...',
+	roomFireDown : '<br>Das Feuer ist erloschen.',
+	roomItem : '<br>Ein Gegenstand auf einem Podest <span id="text-loader2" data-text="f&auml;llt dir auf">f&auml;llt dir auf</span>.',
+	roomItemFound : '<br>Du erreichst das Podest.<br>',
+	roomItemDone : '<br>Das Podest ist leer.',
+	roomEmpty : '<br>Der Raum ist leer...',
+	roomEmptyDoor : '<br>Du entdeckst eine Fallt&uuml;r.<br><br><span id="text-trigger1">Hinabsteigen</span>',
+
+	newFloor1 : 'Die T&uuml;r im Boden f&uuml;hrt weiter in die Tiefe.<br>Du w&uuml;rdest nicht wiederkehren k&ouml;nnen.<br>Es gibt keinen anderen Weg.<br>Ist die Dunkelheit endlos?',
+	newFloor2 : 'Eine weitere T&uuml;r, tiefer in die Minensch&auml;chte.<br>Gefangen von K&auml;lte und D&uuml;sterkeit.<br>Gab es einen Aufzug am Ende der Mine?<br>Das Unheil wartet.',
+	newFloor3 : 'Die dritte T&uuml;r gibt die Sicht auf einen gotischen Steinboden frei.<br><em>Katakomben</em> unter dem Minenkomplex.<br>Eine Chance und ein Risiko, aber ein alternativloses.<br>Du kletterst abw&auml;rts.',
+	newFloor4 : 'Mit einem Klicken &ouml;ffnet sich die Luke.<br><i>Der Weg ist beschwerlich, aber erforderlich</i>,<br>redest du dir stoisch ein.<br>Hoffentlich nicht vergeblich.',
+
+	fightStart : ' beginnt den Kampf.',
+	fightEnemySpella : ' benutzt ',
+	fightEnemySpellb : ' verursacht ',
+	fightEnemySpellc : ' Schaden!<br>',
+
+	fightEnemyDamagea : ' verursacht <span id="fightlog_enemy">',
+	fightEnemyDamageb : '</span> Schaden!<br>',
+
+	fightPlayerDamagea : 'Schlag trifft ',
+	fightPlayerDamageb : ' f&uuml;r <span id="fightlog_player">',
+	fightPlayerDamagec : '</span> Schaden!<br>',
+
+	fightPlayerSpella : ' trifft ',
+	fightPlayerSpellb : ' f&uuml;r ',
+	fightPlayerSpellc : ' Schaden!<br>',
+
+	fightCrit : 'Kritisch!',
+	fightEffective : ' Effektiv!',
+	fightBlock : 'Blockiert!',
+
+	fightPlayerSpellHeala : ' heilt dich  um ',
+	fightPlayerSpellHealb : ' Leben!<br>',
+
+	fightEnemyDead : ' hat aufgeh&ouml;rt zu atmen!</b><br>',
+	fightEnemyFleeDamagea : ' nutzt den Fluchtversuch und verursacht <span id="fightlog_enemy">',
+	fightEnemyFleeDamageb : '</span> Schaden!<br>',
+	fightPlayerFleeSuccess : '<b>Du fliehst vor ',
+	fightPlayerFleeFaila : '<b>Die Flucht vor ',
+	fightPlayerFleeFailb : ' ist unerfolgreich!</b><br>',
+	fightWon : 'Kampf gewonnen!',
+	fightPunch : 'Schlag',
+	fightFlee : 'Fliehen',
+	fightPotion : 'Trank',
+
+	roomEnemyDead : ' liegt tot auf dem Boden.<br>',
+	itemObtained : ' erhalten!',
+	playerSpellLearned : ' erlernt!',
+	playerHealthUp : 'Leben erh&ouml;ht!',
+	playerDamageUp : 'Schaden erh&ouml;ht!',
+	playerPunchUp : '<i>Schlag</i> verbessert!',
+	gameOver : 'Du bist einem schrecklichen Schicksal begegnet,<br>nicht wahr?',
+	alphaOver : 'Vielen Dank f&uuml;r das Testen von <em>CRAWL</em> &alpha;!<br>Diese Version endet hier.',
+
+	enemy0 : 'Graue Ratte',
+	enemy1 : 'Schwarze Ratte',
+	enemy2 : 'Dunkle Ratte',
+	enemy3 : 'Schwarze Spinne',
+	enemy4 : 'Giftige Spinne',
+	enemy5 : 'Rotes Wildschwein',
+	enemy99 : 'Tote Assel',
+	enemy6 : 'Dunkelschlange',
+	enemy7 : 'W&uuml;rgeschlange',
+
+	//Katakomben
+
+	enemy8 : 'Parasitenmensch',
+	enemy9 : 'Gay Boy Berserker',
+
+	//-- Items --
+
+	consumable1 : 'Trank',
+	consumable0 : 'Schl&uuml;ssel',
+	consumable2 : 'Schmerzmittel',
+
+	//Items Minenschacht
+
+	upgrade0 : 'Versorgungspaket', //HP up
+	upgrade1 : 'Stumpfe Klinge', //Dmg up
+	upgrade2 : 'Dunkelpelz', //HP up
+
+	spellItem0 : 'Rosttoxin', //-> Verwunden
+	spellItem1 : 'Sch&uuml;tzendes Brett', //-> Block
+	spellItem2 : 'Stahlhammer', //-> Bruch
+	spellItem3 : 'Gasbrenner', //-> Verbrennen
+
+	//Items Katakomben
+
+	consumable3 : 'Spritze', //Neues Consumable, Zufälliger Effekt. Ersetzt Trank.
+	consumable4 : 'Morphium', //Neues Consumable, sobald es einmal genommen wird sind SPKs nicht mehr wirksam (ersetzt SPKs). Der Spieler wird dazu gezwungen, Morphium zu nehmen. SPK dropchance verringert sich deutlich nach erster Morphiumeinnahme. (SPKs braucht man immernoch für Reliktschreine und Geisteskranke)
+
+	upgrade3 : 'Schlagring', //Erhöht Schlagschaden auf 1.5, gibt Schlag Zersetzungschance.
+	upgrade4 : 'Sch&auml;delhelm', //HP up
+	upgrade5 : 'Notration', //HP up
+
+	spellItem4 : 'Stacheldrahtkeule', //-> Entstellen
+	spellItem5 : 'HV-Batterie', //-> Schock
+
+	gItem0 : 'B&auml;renfalle', //Neuer Itemtyp: G-Item, hält Gegner in Bärenfalle fest, beendet Kampf, cleart den Raum, droppt aber kein Item
+	gItem1 : 'Taschenlampe', //G-Item, Blendet den Gegner, kann für 3 Runden nicht angreifen
+
+	trinket0 : 'Lederhemd', //Neuer Itemtyp: Trinket. Hat passiven Effekt. Panzerweste: Blockchance vs phys. angriff
+	trinket1 : 'Blutaffinit&auml;t', //Trinket. Chance auf Selbstheilung nach gewonnenem Kampf
+	trinket2 : '',
+	trinket3 : '',
+	trinket4 : '',
+
+	//Items Höhlen
+	//Keine neuen Consumables, größere Map
+	gItem2 : '44er Magnum', //G-Item, Onehit
+	gItem3 : 'Bluttransfusion', //G-Item, nur verwendbar unter 50% gegner. HP, tötet sofort, heilt Spieler um fightMaxHealth
+
+	spellItem6 : 'Hochfrequenzgenerator', //-> Schallwelle
+	spellItem7 : 'HV-Generator', //-> Aufladung
+	spellItem8 : 'Leuchtpistole', //-> Blenden
+
+	itemPainkillerTip : 'Dr&uuml;cke V um SPKs zu nehmen.',
+
+	spell0 : 'Schlag', //Phys, Dmg
+	spell1 : 'Block', //Phys, Dmg und Block
+	spell2 : 'Bruch', //Phys, Dmg und Physschwäche
+	spell3 : 'Biss', //Bio, Dmg und Heal
+	spell4 : 'L&auml;hmen', //Phys, Dmg und Stun
+	spell5 : 'Zersetzen', //Bio
+	spell6 : 'Verwunden', //Bio, DoT
+	spell7 : 'Blenden', //Feuer, Dmg und Dunkelzerstörung
+	spell8 : 'Verbrennen', //Feuer, DoT
+	spell9 : 'Entstellen', //Phys, DoT und Stromschwäche
+	spell10 : 'Schock', //Tesla, Dmg und DoTChance
+	spell11 : 'Schallwelle', //Unq, Stun und Stalagmitenchance
+	spell12 : 'Aufladung', //Tesla, 1 Runde aufladen, dann viel Schaden und DoT
+
+	buff0 : '',
+	buff1 : 'Block',
+	buff2 : 'Gegenschlag',
+
+	debuff0 : '',
+	debuff1 : 'Blutung',
+	debuff2 : 'Gebrochen',
+	debuff3 : 'Geblendet',
+	debuff4 : 'Verbrennung',
+	debuff5 : 'Stachelfessel',
+	debuff6 : 'Bet&auml;ubt',
+
+	floorName0 : 'Minensch&auml;chte',
+	floorName1 : 'Katakomben',
+	floorName2 : 'H&ouml;hlen',
+	floorName3 : 'Termina',
+	floorName4 : 'Purgatorium',
+	floorName5 : 'Limbus',
+	floorName6 : 'Babylon',
+
+	enemySpell0 : 'Kratzer',
+	enemySpell1 : 'Biss',
+	enemySpell2 : 'Sammeln',
+	enemySpell3 : 'Heilen'
+}
